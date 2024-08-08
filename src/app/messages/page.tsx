a12f5867fc16acb1471077433ffe455144efab4b
HaimSarraf <ali.assar1988@gmail.com>
2024-08-08 12:00:31 -0400

    End Of Section 2

---
+++ src/app/messages/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MeaagesPage = () => {
+  return <div>MeaagesPage</div>;
+};
+
+export default MeaagesPage;
---
+++ src/components/navabr/TopNav.tsx
@@ -0,0 +1,58 @@
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import Link from "next/link";
+import { GiMatchTip } from "react-icons/gi";
+import NavLink from "./NavLink";
+
+const TopNav = () => {
+  return (
+    <Navbar
+      maxWidth="2xl"
+      className="bg-gradient-to-r from-orange-200 to-red-800"
+      classNames={{
+        item: [
+          "text-l",
+          "text-white",
+          "uppercase",
+          "data-[active=true]:bg-yellow-400",
+          "data-[active=true]:rounded",
+          "data-[active=true]:text-slate-600",
+          "data-[active=true]:font-bold",
+          "data-[active=true]:p-1",
+        ],
+      }}
+    >
+      <NavbarBrand as={Link} href="/">
+        <GiMatchTip size={40} className="text-red-700" />
+        <div className="font-bold text-3xl flex">
+          <span className="text-gray-400">Next</span>
+          <span className="text-red-900">Match</span>
+        </div>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavLink href="/members" label="Members" />
+        <NavLink href="/lists" label="Lists" />
+        <NavLink href="/messages" label="Messages" />
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <Button
+          as={Link}
+          href="/login"
+          variant="bordered"
+          className="text-white"
+        >
+          Login
+        </Button>
+        <Button
+          as={Link}
+          href="/register"
+          variant="bordered"
+          className="text-white"
+        >
+          Register
+        </Button>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default TopNav;
